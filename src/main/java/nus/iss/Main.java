88b768db7cd09c597d6579e6fde2bacea882b6f5
joelsiberia <35606477+joelleeyueer@users.noreply.github.com>
2023-01-17 11:04:29 +0800

    Added basic threading

--- src/main/java/nus/iss/Main.java
+++ src/main/java/nus/iss/Main.java
@@ -3,5 +3,22 @@ package nus.iss;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 5; i++){
+                    System.out.println(Thread.currentThread().getName() + "\tRunnable ..." + i);
+                }
+            }
+        });
+        thread1.start();
+
+        MyRunnableImplementation RI = new MyRunnableImplementation();
+        Thread thread2 = new Thread(RI);
+        thread2.start();
+
+        Thread thread3 = new Thread(RI);
+        thread3.start();
     }
 }
---
+++ src/main/java/nus/iss/MyRunnableImplementation.java
@@ -0,0 +1,20 @@
+package nus.iss;
+
+public class MyRunnableImplementation implements Runnable{
+
+    private String taskName;
+
+    public MyRunnableImplementation() {
+    }
+
+    public MyRunnableImplementation(String taskName) {
+        this.taskName = taskName;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i<5; i++){
+            System.out.println(Thread.currentThread().getName() + "\tRunnable..." + i);
+        }
+    }
+}
